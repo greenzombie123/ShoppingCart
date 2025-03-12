f84bfab07fa38d6f8fd50dba057513097122b7b2
Alvin Hart <105436283+greenzombie123@users.noreply.github.com>
2025-03-12 12:16:55 +0900

    Set up testing framework

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function App({title}:{title:string}) {
   const [count, setCount] = useState(0)
 
   return (
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more {title}
       </p>
     </>
   )
