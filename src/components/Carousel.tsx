2a73f9b48826047bf8fdd55d361d30f4e0dd6bf6
Alvin Hart <105436283+greenzombie123@users.noreply.github.com>
2025-03-24 12:55:58 +0900

    Start creating slider and slide and their tests

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -5,18 +5,56 @@ const Carousel = () => {
     <div className={style.carouselContainer} data-testid="a">
       <button className={style.leftButton}></button>
       <div className={style.carousel_wrapper}>
-                <div className={style.carousel}>
-                    
-                </div>
-                <div className="navigation_list">
-                    <div><a href=""><img src="./assets/dufflebag.webp" alt=""/></a></div>
-                    <div><a href=""><img src="./assets/leatherbag.webp" alt=""/></a></div>
-                    <div><a href=""><img src="./assets/pinkbag.webp" alt=""/></a></div>
-                    <div><a href=""><img src="./assets/leatherbag.webp"alt=""/></a></div>
-                    <div><a href=""><img src="./assets/pinkbag.webp" alt=""/></a></div>
-                </div>
-            </div>
+        <Slider/>
+        <div className="navigation_list">
+          <div>
+            <a href="">
+              <img src="./assets/dufflebag.webp" alt="" />
+            </a>
+          </div>
+          <div>
+            <a href="">
+              <img src="./assets/leatherbag.webp" alt="" />
+            </a>
+          </div>
+          <div>
+            <a href="">
+              <img src="./assets/pinkbag.webp" alt="" />
+            </a>
+          </div>
+          <div>
+            <a href="">
+              <img src="./assets/leatherbag.webp" alt="" />
+            </a>
+          </div>
+          <div>
+            <a href="">
+              <img src="./assets/pinkbag.webp" alt="" />
+            </a>
+          </div>
+        </div>
+      </div>
       <button className={style.rightButton}></button>
+    </div>
+  );
+};
+
+export const Slider = () => {
+  return <div className={style.carousel}>
+    
+  </div>;
+};
+
+export const Slide = () => {
+  return (
+    <div className={style.slide} data-testId="slide">
+      <img src="" alt="" />
+      <div className={style.itemInfo}>
+        <div className={style.name}>
+          leather bag f2fwefw
+        </div>
+        <div className={style.price}>$100</div>
+      </div>
     </div>
   );
 };
--- src/tests/Carousel.test.tsx
+++ src/tests/Carousel.test.tsx
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
-import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import { createMemoryRouter, RouteObject, RouterProvider } from "react-router-dom";
 import { render, screen, waitFor } from "@testing-library/react";
-import Carousel from "../components/Carousel";
+import Carousel, { Slider } from "../components/Carousel";
 import App from "../App";
 import routes from "../routes";
 
@@ -17,5 +17,24 @@ describe("Carousel", () => {
     expect(buttons[1]).toBeInTheDocument();
   });
 
-  // it()
+  it("render a slide of a product", async ()=>{
+    const container = render(<Slider/>)
+
+    const slide = await waitFor(() => screen.getByTestId("slide"));
+
+    expect(slide).toBeInTheDocument();
+  })
+
+  it("renders all 5 slides",async ()=>{
+
+   render(<Slider/>)
+
+    const slides = await waitFor(() => screen.getAllByRole("link"));
+
+    expect(slides[0]).toBeInTheDocument();
+    expect(slides[1]).toBeInTheDocument();
+    expect(slides[2]).toBeInTheDocument();
+    expect(slides[3]).toBeInTheDocument();
+    expect(slides[4]).toBeInTheDocument();
+  })
 });
